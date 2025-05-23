39f13ff6f3544a891f73da0c620742327321d5bf
Jeanhefler <jeanhefler@outlook.com>
2025-05-23 00:02:05 -0300

    first commit

---
+++ src/main/java/com/jeanhefler/adoptme/AdoptmeApplication.java
@@ -0,0 +1,13 @@
+package com.jeanhefler.adoptme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptmeApplication.class, args);
+	}
+
+}
